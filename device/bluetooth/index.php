834df635e39c1ac48dd756fcf878e055bed22bc4
sunyiminglove <sunyiming@sunyimingdeMacBook-Pro.local>
2017-08-09 23:17:24 +0800

    蓝牙版本V1.3
    1.完成单个设置界面
    2.可以更改按键值，按键名
    3.可以更改按键图片，替换图片URL，并实时显示预览

--- device/bluetooth/index.php
+++ device/bluetooth/index.php
@@ -12,23 +12,22 @@ include "../../public/header.php";
 <main class="contain">
 	<table>
 	  <tr>
-	    <th>编号</th>
-	    <th>名称</th>
-	    <th>按下指令</th>
-	    <th>抬起指令</th>
-	    <th>按下图片</th>
-	    <th>抬起图片</th>
+	    <th style="width: 10%">编号</th>
+	    <th style="width: 10%">名称</th>
+	    <th style="width: 15%">按下指令</th>
+	    <th style="width: 15%">抬起指令</th>
+	    <th style="width: 10%">预览</th>
+	    <th style="width: 20%">链接</th>
 	  </tr>
-
 	  <?php 
 	  for ($i=0; $i < 12; $i++) { 
 	  	echo "<tr>
 			    <td>$i</td>
-			    <td><input type='text' id='a$i'></td>
-			    <td><input type='text' id='b$i'></td>
-			    <td><input type='text' id='c$i'></td>
-			    <td><input type='text' id='d$i'></td>
-			    <td><input type='text' id='e$i'></td>
+			    <td><input type='text' id='a$i' style='width:95%;height:90%;'></td>
+			    <td><input type='text' id='b$i' style='width:95%;height:90%;'></td>
+			    <td><input type='text' id='c$i' style='width:95%;height:90%;'></td>
+			    <td><img id='img$i' src='http://www.easyicon.net/api/resizeApi.php?id=1206083&size=128' style='width: 95%;height:auto; '></td>
+			    <td><input type='text' id='e$i' style='width:95%;height:90%;'></td>
 			  </tr>";
 	  }
 	  ?>
@@ -39,36 +38,42 @@ include "../../public/header.php";
 </html>
 
 <script type="text/javascript">
-	//监控输入值变化并打印到控制台 
-	$("#a0").bind("change", function(){ console.log("msg:%s", $("#a0").val()); });
-
-	$("#b0").bind("change", function(){ console.log("msg:%s", $("#b0").val()); });
-
-	// 上传到服务器
-	// 选择遥控器，上面三个大按钮，是遥控器的缩略图，底下部分能自动切换
-
-
-    // $("input:button").click(function() {
-
-    //     str = $(this).val()=="编辑"?"确定":"编辑";  
-
-    //     $(this).val(str);   // 按钮被点击后，在“编辑”和“确定”之间切换
-
-    //     $(this).parent().siblings("td").each(function() {  // 获取当前行的其他单元格
-
-    //         obj_text = $(this).find("input:text");    // 判断单元格下是否有文本框
-
-    //         if(!obj_text.length)   // 如果没有文本框，则添加文本框使之可以编辑
-
-    //             $(this).html("<input type='text' value='"+$(this).text()+"'>");
-
-    //         else   // 如果已经存在文本框，则将其显示为文本框修改的值
-
-    //             $(this).html(obj_text.val()); 
-
-    //     });
-
-    // });
-
-// }); 
+	// 图片链接变化时赋给预览图
+	$("#e0").bind("change", function(){ 
+		// console.log("msg:%s", $("#e0").val()); 
+		$("#img0").attr("src",$("#e0").val());
+	});
+	$("#e1").bind("change", function(){
+		$("#img1").attr("src",$("#e1").val());
+	});
+	$("#e2").bind("change", function(){ 
+		$("#img2").attr("src",$("#e2").val());
+	});
+	$("#e3").bind("change", function(){ 
+		$("#img3").attr("src",$("#e3").val());
+	});
+	$("#e4").bind("change", function(){ 
+		$("#img4").attr("src",$("#e4").val());
+	});
+	$("#e5").bind("change", function(){ 
+		$("#img5").attr("src",$("#e5").val());
+	});
+	$("#e6").bind("change", function(){ 
+		$("#img6").attr("src",$("#e6").val());
+	});
+	$("#e7").bind("change", function(){ 
+		$("#img7").attr("src",$("#e7").val());
+	});
+	$("#e8").bind("change", function(){ 
+		$("#img8").attr("src",$("#e8").val());
+	});
+	$("#e9").bind("change", function(){ 
+		$("#img9").attr("src",$("#e9").val());
+	});
+	$("#e10").bind("change", function(){ 
+		$("#img10").attr("src",$("#e10").val());
+	});
+	$("#e11").bind("change", function(){ 
+		$("#img11").attr("src",$("#e11").val());
+	});
 </script>
