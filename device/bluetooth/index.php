a49d2d1c7a86033beb955903c32976c41dce450f
公司电脑 <sunyiming537@126.com>
2017-08-11 11:04:18 +0800

    蓝牙遥控V1.4
    1.增加切换功能
    2.优化了事件触发代码

--- device/bluetooth/index.php
+++ device/bluetooth/index.php
@@ -10,70 +10,110 @@ include "../../public/header.php";
 </head>
 <body>
 <main class="contain">
-	<table>
-	  <tr>
-	    <th style="width: 10%">编号</th>
-	    <th style="width: 10%">名称</th>
-	    <th style="width: 15%">按下指令</th>
-	    <th style="width: 15%">抬起指令</th>
-	    <th style="width: 10%">预览</th>
-	    <th style="width: 20%">链接</th>
-	  </tr>
-	  <?php 
-	  for ($i=0; $i < 12; $i++) { 
-	  	echo "<tr>
-			    <td>$i</td>
-			    <td><input type='text' id='a$i' style='width:95%;height:90%;'></td>
-			    <td><input type='text' id='b$i' style='width:95%;height:90%;'></td>
-			    <td><input type='text' id='c$i' style='width:95%;height:90%;'></td>
-			    <td><img id='img$i' src='http://www.easyicon.net/api/resizeApi.php?id=1206083&size=128' style='width: 95%;height:auto; '></td>
-			    <td><input type='text' id='e$i' style='width:95%;height:90%;'></td>
-			  </tr>";
-	  }
-	  ?>
-	</table>
+	<ul id="myTab" class="nav nav-tabs">
+		<li class="active">
+			<a href="#config1" data-toggle="tab">配置1</a>
+		</li>
+		<li>
+			<a href="#config2" data-toggle="tab">配置2</a>
+		</li>
+		<li>
+			<a href="#config3" data-toggle="tab">配置3</a>
+		</li>
+	</ul>
+
+	<div id="myTabContent" class="tab-content">
+		<div class="tab-pane fade in active" id="config1">
+			<p style="font-size: 25px;">配置1</p>
+			<table>
+			  <tr>
+			    <th style="width: 10%">编号</th>
+			    <th style="width: 10%">名称</th>
+			    <th style="width: 15%">按下指令</th>
+			    <th style="width: 15%">抬起指令</th>
+			    <th style="width: 10%">预览</th>
+			    <th style="width: 20%">链接</th>
+			  </tr>
+			  <?php 
+			  for ($i=0; $i < 12; $i++) { 
+			  	echo "<tr>
+					    <td>$i</td>
+					    <td><input type='text' id='a1$i' style='width:95%;height:90%;'></td>
+					    <td><input type='text' id='b1$i' style='width:95%;height:90%;'></td>
+					    <td><input type='text' id='c1$i' style='width:95%;height:90%;'></td>
+					    <td><img id='img1$i' src='http://www.easyicon.net/api/resizeApi.php?id=1206083&size=128' style='width: 95%;height:auto; '></td>
+					    <td><input type='text' id='e1$i' style='width:95%;height:90%;'></td>
+					  </tr>";
+			  }
+			  ?>
+			</table>
+		</div>
+		<div class="tab-pane fade" id="config2">
+			<p style="font-size: 25px;">配置2</p>
+			<table>
+			  <tr>
+			    <th style="width: 10%">编号</th>
+			    <th style="width: 10%">名称</th>
+			    <th style="width: 15%">按下指令</th>
+			    <th style="width: 15%">抬起指令</th>
+			    <th style="width: 10%">预览</th>
+			    <th style="width: 20%">链接</th>
+			  </tr>
+			  <?php 
+			  for ($i=0; $i < 12; $i++) { 
+			  	echo "<tr>
+					    <td>$i</td>
+					    <td><input type='text' id='a2$i' style='width:95%;height:90%;'></td>
+					    <td><input type='text' id='b2$i' style='width:95%;height:90%;'></td>
+					    <td><input type='text' id='c2$i' style='width:95%;height:90%;'></td>
+					    <td><img id='img2$i' src='http://www.easyicon.net/api/resizeApi.php?id=1206083&size=128' style='width: 95%;height:auto; '></td>
+					    <td><input type='text' id='e2$i' style='width:95%;height:90%;'></td>
+					  </tr>";
+			  }
+			  ?>
+			</table>
+		</div>
+		<div class="tab-pane fade" id="config3">
+			<p style="font-size: 25px;">配置3</p>
+			<table>
+			  <tr>
+			    <th style="width: 10%">编号</th>
+			    <th style="width: 10%">名称</th>
+			    <th style="width: 15%">按下指令</th>
+			    <th style="width: 15%">抬起指令</th>
+			    <th style="width: 10%">预览</th>
+			    <th style="width: 20%">链接</th>
+			  </tr>
+			  <?php 
+			  for ($i=0; $i < 12; $i++) { 
+			  	echo "<tr>
+					    <td>$i</td>
+					    <td><input type='text' id='a3$i' style='width:95%;height:90%;'></td>
+					    <td><input type='text' id='b3$i' style='width:95%;height:90%;'></td>
+					    <td><input type='text' id='c3$i' style='width:95%;height:90%;'></td>
+					    <td><img id='img3$i' src='http://www.easyicon.net/api/resizeApi.php?id=1206083&size=128' style='width: 95%;height:auto; '></td>
+					    <td><input type='text' id='e3$i' style='width:95%;height:90%;'></td>
+					  </tr>";
+			  }
+			  ?>
+			</table>
+		</div>
+	</div>
 </main>
 	
 </body>
 </html>
 
 <script type="text/javascript">
-	// 图片链接变化时赋给预览图
-	$("#e0").bind("change", function(){ 
-		// console.log("msg:%s", $("#e0").val()); 
-		$("#img0").attr("src",$("#e0").val());
-	});
-	$("#e1").bind("change", function(){
-		$("#img1").attr("src",$("#e1").val());
-	});
-	$("#e2").bind("change", function(){ 
-		$("#img2").attr("src",$("#e2").val());
-	});
-	$("#e3").bind("change", function(){ 
-		$("#img3").attr("src",$("#e3").val());
-	});
-	$("#e4").bind("change", function(){ 
-		$("#img4").attr("src",$("#e4").val());
-	});
-	$("#e5").bind("change", function(){ 
-		$("#img5").attr("src",$("#e5").val());
-	});
-	$("#e6").bind("change", function(){ 
-		$("#img6").attr("src",$("#e6").val());
-	});
-	$("#e7").bind("change", function(){ 
-		$("#img7").attr("src",$("#e7").val());
-	});
-	$("#e8").bind("change", function(){ 
-		$("#img8").attr("src",$("#e8").val());
-	});
-	$("#e9").bind("change", function(){ 
-		$("#img9").attr("src",$("#e9").val());
-	});
-	$("#e10").bind("change", function(){ 
-		$("#img10").attr("src",$("#e10").val());
-	});
-	$("#e11").bind("change", function(){ 
-		$("#img11").attr("src",$("#e11").val());
-	});
+	//图片链接变化时赋给预览图
+	$('[id^="e"]').bind("input propertychange",function(){
+		//打印引起事件的标签信息
+  		console.log('change:', this);
+  		//打印ID号
+  		console.log('change:', $(this).attr('id'));
+  		var imgid = $(this).attr('id').replace('e','#img');
+  		//将当前值赋给预览框
+  		 $(imgid).attr("src",$(this).val());
+	});	
 </script>
+	
