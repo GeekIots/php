04aa7d3de85cf7d44a6209a3a68726db5a0c729c
MacBook Pro <15339287330@126.com>
2017-08-25 01:36:07 +0800

    V1.5.9  更正同步造成的异常

--- public/upload-head/js/cropbox.js
+++ public/upload-head/js/cropbox.js
@@ -154,7 +154,7 @@
 
 // 更新预览
 function Preview(obj) {
-    var img = obj.getDataURL();
+    var img = cropper.getDataURL();
     $('.cropped').html('');
     $('.cropped').append('<img src="'+img+'" align="absmiddle" style="width:64px;margin-top:4px;border-radius:64px;box-shadow:0px 0px 12px #7E7E7E;" ><p>64px*64px</p>');
     $('.cropped').append('<img src="'+img+'" align="absmiddle" style="width:128px;margin-top:4px;border-radius:128px;box-shadow:0px 0px 12px #7E7E7E;"><p>128px*128px</p>');
